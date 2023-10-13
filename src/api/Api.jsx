8fcbdc97f18f28584c33783c9ed346f9d94b9e8d
vishnukkrishna <vishnukkrishna1999@gmail.com>
2023-10-13 19:06:15 +0530

    base url change

--- src/api/Api.jsx
+++ src/api/Api.jsx
@@ -1,2 +1,2 @@
-export const BACKEND_BASE_URL = "http://localhost:8000";
-export const wsApiUrl = 'ws://127.0.0.1:8000';
+export const BACKEND_BASE_URL = "https://staffsense.onrender.com/";
+export const wsApiUrl = 'ws://staffsense.onrender.com/';
