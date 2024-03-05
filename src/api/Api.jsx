14d3c870fb188b2b9e3360b87524f8f46fa692e6
Vishnu Krishnakumar <83391274+vishnukkrishna@users.noreply.github.com>
2024-03-05 22:38:35 +0530

    navbar color changed

--- src/api/Api.jsx
+++ src/api/Api.jsx
@@ -1,5 +1,5 @@
-export const BACKEND_BASE_URL = "http://localhost:8000";
-export const wsApiUrl = "ws://127.0.0.1:8000";
+// export const BACKEND_BASE_URL = "http://localhost:8000";
+// export const wsApiUrl = "ws://127.0.0.1:8000";
 
-// export const BACKEND_BASE_URL = "https://staffsense.onrender.com";
-// export const wsApiUrl = "wss://staffsense.onrender.com";
+export const BACKEND_BASE_URL = "https://staffsense.onrender.com";
+export const wsApiUrl = "wss://staffsense.onrender.com";
